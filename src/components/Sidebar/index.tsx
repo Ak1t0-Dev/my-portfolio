4f71ae39fcc7e5ec292ace32c8aca85e351614be
AkitoTobita <t0bitakito.001@gmail.com>
2023-09-26 15:11:55 -0700

    update: added a close button

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -13,6 +13,7 @@ import {
 import { logo, sidebarItems } from "../../constants/sidebarItems";
 import { useNavigate } from "react-router-dom";
 import MenuRoundedIcon from "@mui/icons-material/MenuRounded";
+import CloseIcon from "@mui/icons-material/Close";
 import { useState } from "react";
 
 export const Sidebar = () => {
@@ -100,6 +101,15 @@ export const Sidebar = () => {
           },
         }}
       >
+        <CloseIcon
+          onClick={handleMenuToggle}
+          sx={{
+            position: "absolute",
+            top: "20px",
+            right: "20px",
+            color: "#393e46",
+          }}
+        />
         {drawer}
       </Drawer>
       <Box sx={{ display: { xs: "none", md: "block" } }}>{drawer}</Box>
